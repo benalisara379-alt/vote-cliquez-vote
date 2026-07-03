ecaf26ee1d167fa67c790fa431fb1e8604adf050
benalisara379-alt <benalisara379@gmail.com>
2026-07-04 01:57:19 +0200

    Add index.php to public

--- public/index.php
+++ public/index.php
@@ -1,15 +1,2 @@
 <?php
-
-require __DIR__.'/vendor/autoload.php';
-
-$app = require_once __DIR__.'/bootstrap/app.php';
-
-use Illuminate\Http\Request;
-
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
-$response = $kernel->handle(
-    $request = Request::capture()
-)->send();
-
-$kernel->terminate($request, $response);
+echo "Hello from public!";
