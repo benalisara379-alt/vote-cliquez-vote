92fd9e7ce6bc2105bb79c772bccecd9f83cf19b1
benalisara379-alt <benalisara379@gmail.com>
2026-07-04 01:18:35 +0200

    Fix index.php in public

--- public/index.php
+++ public/index.php
@@ -4,10 +4,12 @@ require __DIR__.'/vendor/autoload.php';
 
 $app = require_once __DIR__.'/bootstrap/app.php';
 
+use Illuminate\Http\Request;
+
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
 $response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
+    $request = Request::capture()
 )->send();
 
 $kernel->terminate($request, $response);
